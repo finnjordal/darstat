1ed1de5bd3f32f957858e596eff836aca1684de5
finnjordal <finn@jordal.dk>
2018-02-05 12:58:33 +0100

    streaming

--- darstat.js
+++ darstat.js
@@ -1,7 +1,10 @@
 
 var program = require('commander')
-	,rp = require('request-promise')
-	,moment = require('moment');
+//	,rp = require('request-promise')
+	,moment = require('moment')
+	,util = require('util')
+  , JSONStream = require('JSONStream') 
+	, request= require('request');
 
 function range(val) {
   return val.split('..').map(Number);
@@ -38,8 +41,8 @@ if (program.døgn) {
 		program.outputHelp();		
 		process.exit(1);
 	}
-	fra= moment({year: år, month: 1, day: 1, hour: 0, minute: 0, second: 0, millisecond: 0});;
-	til= moment({year: år+1, month: 1, day: 1, hour: 0, minute: 0, second: 0, millisecond: 0});; 
+	fra= moment({year: år, month: 0, day: 1, hour: 0, minute: 0, second: 0, millisecond: 0});;
+	til= moment({year: år+1, month: 0, day: 1, hour: 0, minute: 0, second: 0, millisecond: 0});; 
 } else if (program.periode) {
 	
 	fra= moment(program.periode[0],'YYYYMMDD');
@@ -63,8 +66,8 @@ if (program.døgn) {
 	}
 }
 
-console.log('antal døgn: %d', antaldøgn);
-console.log('%s - %s', fra.utc().toISOString(), til.utc().toISOString());
+//console.log('antal døgn: %d', antaldøgn);
+//console.log('%s - %s', fra.utc().toISOString(), til.utc().toISOString());
 
 console.log('Periode: %s - %s',fra.utc().toISOString(), til.utc().toISOString());
 
@@ -72,50 +75,92 @@ var vejoption= {};
     vejoption.baseUrl= host;
     vejoption.url='/replikering/vejstykker/haendelser';
     vejoption.qs= {tidspunktfra: fra.utc().toISOString(), tidspunkttil: til.utc().toISOString()};
-var vejrequest= rp(vejoption);
+var vejrequest= request(vejoption);
+//console.log('uri: ' + vejrequest.uri.href);
 
 var adresseoption= {};
     adresseoption.baseUrl= host;
     adresseoption.url='/replikering/adresser/haendelser';
     adresseoption.qs= {tidspunktfra: fra.utc().toISOString(), tidspunkttil: til.utc().toISOString()};
-var adresserequest= rp(adresseoption);
+var adresserequest= request(adresseoption);
+//console.log('uri: ' + adresserequest.uri.href);
 
 var adgangsadresseoption= {};
     adgangsadresseoption.baseUrl= host;
     adgangsadresseoption.url='/replikering/adgangsadresser/haendelser';
     adgangsadresseoption.qs= {tidspunktfra: fra.utc().toISOString(), tidspunkttil: til.utc().toISOString()};
-var adgangsadresserequest= rp(adgangsadresseoption);
+var adgangsadresserequest= request(adgangsadresseoption);
+//console.log('uri: ' + adgangsadresserequest.uri.href);
+
+//---------------------------------------------------------------------------------------------------------------
+// jsonparser
+//
+function jsonparser(navn) {
+	var parser= JSONStream.parse('*');
+
+	parser.on('error', function (obj) {
+  	console.log('%s - JSONparser error: %s', navn, obj);
+	});
+
+	return parser;
+}
+
+//-----------------------------------------------------------------------------------------------------------------
+// Analyse
+//
+var start= Date.now();
 
 
-Promise.all([vejrequest, adgangsadresserequest, adresserequest])
-	.then(function(svar) {
-		vejnavne= JSON.parse(svar[0]);
-		const vejnavnestat = new Map();
-		vejnavne.forEach(function(vejnavn) {
-			const count = vejnavnestat.get(vejnavn.operation) || 0;
-    	vejnavnestat.set(vejnavn.operation, count + 1);
-			//console.log("%s %s %s (%s), %s (%s)" ,moment(vejnavn.tidspunkt).local().format('LLL'),vejnavn.operation,vejnavn.data.navn,vejnavn.data.kode,vejnavn.data.kommunenavn,vejnavn.data.kommunekode);
-		})
-		console.log('Vejstykker: insert: %d, update: %d, delete: %d', vejnavnestat.get('insert')||0, vejnavnestat.get('update')||0, vejnavnestat.get('delete')||0);
-		
-		adgangsadresser= JSON.parse(svar[1]);
-		const adgangsadresserstat = new Map();
-		adgangsadresser.forEach(function(vejnavn) {
-			const count = adgangsadresserstat.get(vejnavn.operation) || 0;
-    	adgangsadresserstat.set(vejnavn.operation, count + 1);
-			//console.log("%s %s %s (%s), %s (%s)" ,moment(vejnavn.tidspunkt).local().format('LLL'),vejnavn.operation,vejnavn.data.navn,vejnavn.data.kode,vejnavn.data.kommunenavn,vejnavn.data.kommunekode);
-		})
-		console.log('Adgangsadresser: insert: %d, update: %d, delete: %d', adgangsadresserstat.get('insert')||0, adgangsadresserstat.get('update')||0, adgangsadresserstat.get('delete')||0);
-		
-		adresser= JSON.parse(svar[2]);
-		const adresserstat = new Map();
-		adresser.forEach(function(vejnavn) {
-			const count = adresserstat.get(vejnavn.operation) || 0;
-    	adresserstat.set(vejnavn.operation, count + 1);
-			//console.log("%s %s %s (%s), %s (%s)" ,moment(vejnavn.tidspunkt).local().format('LLL'),vejnavn.operation,vejnavn.data.navn,vejnavn.data.kode,vejnavn.data.kommunenavn,vejnavn.data.kommunekode);
-		})
-		console.log('Adresser: insert: %d, update: %d, delete: %d', adresserstat.get('insert')||0, adresserstat.get('update')||0, adresserstat.get('delete')||0);
-	})
-	.catch(function(error) {	  			
-		console.log('all Fejl: '+error);
-	})
+const { Transform } = require('stream');
+
+class StatTransform extends Transform {
+  constructor(options) {
+    super(options);
+    this.stat= new Map();
+    this.statname= options.statname;
+    this.antal= 0;
+  }
+  _transform(chunk, encoding, callback) {
+  	const count = this.stat.get(chunk.operation) || 0;
+   	this.stat.set(chunk.operation, count + 1);
+  	this.antal++;
+		//this.push(util.format("%s - %d: %s\n\r",this.statname, this.antal, chunk.operation));
+		callback();
+  }
+  _final(callback) {
+  	const tekst= util.format('%s: insert: %d, update: %d, delete: %d\n\r', this.statname, this.stat.get('insert')||0, this.stat.get('update')||0, this.stat.get('delete')||0);
+  	this.push(tekst);
+		callback();
+  }
+}
+
+//-----------------------------------------------------------------------------------------------------------------
+// Analyser
+//
+
+function analyse(navn) {
+	var stat = new StatTransform({objectMode: true, statname: navn});
+
+	// stat.on('end', function (obj) {
+	//  	console.log("%s - antal: %d, Tid: %d ms", stat.statname, stat.antal, Date.now()-start);
+	// });
+
+	// stat.on('finish', function (obj) {
+	//   console.log("%s - finish: Antal: %d, Tid: %d ms", stat.statname, stat.antal, Date.now()-start);
+	// });
+
+	stat.on('error', function (err) {
+	  console.log("%s - analyse error: %s", stat.statname, err);
+	});
+
+	return stat;
+}
+
+//console.log('før pipe');
+
+//-----------------------------------------------------------------------------------------------------------------
+// pipes
+//
+vejrequest.pipe(jsonparser('Vejstykker')).pipe(analyse('Vejstykker')).pipe(process.stdout);
+adresserequest.pipe(jsonparser('Adresser')).pipe(analyse('Adresse')).pipe(process.stdout);
+adgangsadresserequest.pipe(jsonparser('Adgangsadresser')).pipe(analyse('Adgangsadresser')).pipe(process.stdout);
